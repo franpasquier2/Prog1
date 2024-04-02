72d38f7cad16bb2f88232dbe6ac9721706e8878e
delaVegaLeonel <l.delavega@alumno.um.edu.ar>
2024-04-02 14:07:30 -0300

    modificaciones del tp

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from main import creat_app
 import os
-from dotenv import load_dotenv
-load_dotenv()
-app = Flask(__name__)
+
+app = creat_app()
+
+app.app_context().push()
+
 if __name__ == '__main__':
     app.run(debug=True,port=os.getenv("PORT"))
