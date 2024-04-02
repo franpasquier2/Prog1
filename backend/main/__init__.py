3b5930a10d05d560fbac5a81bc7598a8b4059539
delaVegaLeonel <l.delavega@alumno.um.edu.ar>
2024-04-02 14:11:32 -0300

    commits tp

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from dotenv import load_dotenv
 
 
-#Este metodo create_app inicializa la app y todos los modulos
 def creat_app():
     app = Flask(__name__)
     load_dotenv()
-    
+
     return app
