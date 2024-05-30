3ce67fbfa4f04c09b54ca399491470da2cf0ec74
Francisco Pasquier <franciscopasquier@pop-os.localdomain>
2024-05-30 17:23:51 -0300

    TP6 realizado

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -3,25 +3,22 @@ from dotenv import load_dotenv
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 api = Api()
-
 db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-
     load_dotenv()
-
-
-
+    
     if not os.path.exists(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')):
         os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
-        
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
     db.init_app(app)
 
@@ -44,8 +41,16 @@ def create_app():
     api.add_resource(resources.AutoresResources, '/autores')
     api.add_resource(resources.AutorResources, '/autor/<id>')
 
-    
     api.init_app(app)
 
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    #Cargar tiempo de expiración de los tokens
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES'))
+    jwt.init_app(app)
+
+    from main.auth import routes
+    #Importar blueprint
+    app.register_blueprint(routes.auth)
+    
 
     return app
--- backend/main/models/usuario.py
+++ backend/main/models/usuario.py
@@ -1,11 +1,15 @@
 from .. import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 class Usuario(db.Model):
     id_usuario = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
     name = db.Column(db.String(100), nullable=False)
+    #Mail usado como nombre de usuario
+    email = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    #Contraseña que será el hash de la pass en texto plano
+    password = db.Column(db.String(128), nullable=False)
+    #Rol (En el caso que existan diferentes tipos de usuarios/Usuarioes con diferentes permisos)
+    rol = db.Column(db.String(10), nullable=False, server_default="users")
     
 #Relacion un usuario a muchos libros
     libros = db.relationship("Libro" ,back_populates = "usuario", cascade="all, delete-orphan")
@@ -13,6 +17,19 @@ class Usuario(db.Model):
     prestamos = db.relationship("Prestamo" ,back_populates = "usuario", cascade="all, delete-orphan")
 #Relacion un usuario a muchas notificaciones
     notificaciones = db.relationship("Notificacion" ,back_populates = "usuario", cascade="all, delete-orphan")
+
+#Un 'hash' es el resultado de aplicar una función matemática que toma una entrada y la transforma en una cadena de caracteres, generalmente una representación alfanumérica de longitud fija de los datos de entrada.
+    @property
+    def plain_password(self): # plain_password evita que se pueda acceder a la contrasena en texto plano 
+        raise AttributeError('Password cant be read')
+    #Setter de la contraseña toma un valor en texto plano
+    # calcula el hash y lo guarda en el atributo password
+    @plain_password.setter
+    def plain_password(self, password):
+        self.password = generate_password_hash(password)
+    #Método que compara una contraseña en texto plano con el hash guardado en la db
+    def validate_pass(self,password):
+        return check_password_hash(self.password, password)
     
     def _repr_(self):                    
         return '<Usuario: %r >' % (self.name)
@@ -23,7 +40,8 @@ class Usuario(db.Model):
             'username': str(self.username),
             'password': self.password,
             'email': str(self.email),
-            'name': str(self.name),            
+            'name': str(self.name),
+            'rol': str(self.rol),           
             # No incluimos la contraseña por seguridad
         }
         return usuario_json
@@ -55,9 +73,13 @@ class Usuario(db.Model):
         password = Usuario_json.get('password')
         email = Usuario_json.get('email')
         name = Usuario_json.get('name')
+        password = Usuario_json.get('password')
+        rol = Usuario_json.get('rol')
         return Usuario(id_usuario=id_usuario,
                     username=username,
                     password=password,
                     email=email,
                     name=name,
+                    plain_password=password,
+                    rol=rol,
                     )
